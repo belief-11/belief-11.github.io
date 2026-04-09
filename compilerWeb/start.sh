9491790844fbdcbabd1fa5e3ec8f5ac776e54b5e
belief-11 <2330528357@qq.com>
2026-04-09 09:54:04 +0800

    Delete compilerWeb directory

--- compilerWeb/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Starting local server..."
-echo ""
-echo "Server will be available at: http://localhost:8000"
-echo "Press Ctrl+C to stop the server"
-echo ""
-python3 -m http.server 8000
